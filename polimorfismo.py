4a6eee97d05d25c1f07b671fd34c2c4ed2600dd6
Daniel M Castillo <danielalejandromoralescastillo@gmail.com>
2025-04-24 00:53:43 -0600

    Polimorfismo

---
+++ polimorfismo.py
@@ -0,0 +1,24 @@
+#Polimorfismo test
+class Ave:
+    def volar(self):
+        pass
+    
+class Aguila(Ave):
+    def volar(self):
+        return "El aguila vuela alto"
+    
+class Pinguino(Ave):
+    def volar(self):
+        return "El pinguino no puede volar"
+    
+aguila = Aguila()
+pinguino = Pinguino()
+
+aves = [aguila,pinguino]
+
+def hacer_volar(ave):
+    print(ave.volar())
+    
+for i in range (len(aves)):
+    hacer_volar(aves[i])
+    
