58dfa3ed9558a615921c6c1fa5f7cbc51a3281de
Daniel M Castillo <danielalejandromoralescastillo@gmail.com>
2025-04-24 00:53:20 -0600

    Herencia

---
+++ herencia.py
@@ -0,0 +1,20 @@
+#Herencia Test
+class Animal:
+    def __init__(self, nombre):
+        self.nombre = nombre
+    
+    def sonido(self):
+        pass
+    
+class Perro(Animal):
+    def sonido(self):
+        return("Guau")
+
+class Gato(Animal):
+    def sonido(self):
+        return("Miau")
+
+perro = Perro("Max")
+gato = Gato("Michi")
+print(perro.nombre, "dice", perro.sonido())
+print(gato.nombre, "dice", gato.sonido())
